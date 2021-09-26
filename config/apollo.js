576d502ef7b3e0b4fd0dac3eba488a93fbbe6892
Goomerr <63950956+Goomerr@users.noreply.github.com>
2021-09-26 17:21:55 +0200

    Update apollo.js

--- config/apollo.js
+++ config/apollo.js
@@ -3,7 +3,7 @@ import fetch from 'node-fetch';
 import { setContext } from 'apollo-link-context';
 
 const httpLink = createHttpLink({
-    uri: 'https://safe-atoll-98733.herokuapp.com/',
+    uri: 'https://goomergraphqlcrm.herokuapp.com/',
     fetch
 });
 
